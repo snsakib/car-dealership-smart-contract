303f7d93aea6c5074222647c3525df567f118aec
snsakib <hello@snsakib.com>
2023-06-28 01:58:58 +0600

    setup properties

--- contracts/CarDealership.sol
+++ contracts/CarDealership.sol
@@ -6,6 +6,21 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract CarDealership is ERC721URIStorage {
 
+  address payable contractOwner;
 
-  constructor() ERC721("CarDealership", "CARNFT") {}
+  struct Car {
+    uint256 id;
+    address payable owner;
+    address payable seller;
+    string brand;
+    uint256 price;
+    uint256 mileage;
+    bool isListedForSale;
+  }
+
+  mapping(uint256 => Car) private idToCar;
+
+  constructor() ERC721("CarDealership", "CARNFT") {
+    contractOwner = payable(msg.sender);
+  }
 }
